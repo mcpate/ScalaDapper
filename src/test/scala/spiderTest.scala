70c687a3035ea46ef08e988d7c3107b1f9f8af0d
MCPate <mcpate@gmail.com>
2013-09-24 16:20:24 -0700

    Cleans up test and code. Everything now working. This is a 'rough' full implementation of the Spider Pattern.

--- src/test/scala/spiderTest.scala
+++ src/test/scala/spiderTest.scala
@@ -28,14 +28,15 @@ class Transformer(next: ActorRef) extends Actor with Node {
 	def receive = {
 		case m: SomeMessage =>
 			println("Transformer received one...")
-			send(next, m.copy(text = (m.text.head.toUpper +: m.text.tail).toString ))
+			next ! m.copy() 
+		case _ => println("Transformer Actor: case _ match")
 	}
 }
 
 class Printer extends Actor {
 	def receive = {
 		case m: SomeMessage => println( m.text )
-		case _ =>
+		case _ => println("case _ match in Printer Actor")
 	}
 }
 
@@ -60,18 +61,10 @@ class SpiderTest extends TestKit(ActorSystem("spider")) with WordSpecLike with S
 			//val actor3 = system.actorOf(Props(createDiagnostic), "t-3")
 			// create an Iterable to initialize the chosen router with
 			//val routees = Vector[ActorRef](actor1, actor2, actor3)
-
-			val actor4 = system.actorOf(Props[Printer], "t-4")
-			val actor5 = system.actorOf(Props[Printer], "t-5")
-			val actor6 = system.actorOf(Props[Printer], "t-6")
-			val routees2 = Vector[ActorRef](actor4, actor5, actor6)
 			
-
 			// put them in a specific type of router. BroadcastRouter "broadcasts" messages to all actors within.
-			val router = system.actorOf(Props[Printer].withRouter(
-				BroadcastRouter(routees = routees2)), "router-to-transformers")
-/*			val router = system.actorOf(Props(createDiagnostic).withRouter(
-				BroadcastRouter(routees = routees)), "router-to-transformers")
+			//val router = system.actorOf(Props(createDiagnostic).withRouter(
+			//	BroadcastRouter(routees = routees)), "router-to-transformers")
 			//val transformerWithRouter = system.actorOf(Props(new Transformer(router) with TimingDiagnostics), "transformer-with-router")
 			//val transformer = system.actorOf(Props(new Transformer(transformerWithRouter) with TimingDiagnostics), "first-transformer")
 
@@ -79,39 +72,50 @@ class SpiderTest extends TestKit(ActorSystem("spider")) with WordSpecLike with S
 			// transformer --> transformerWithRouter --> 	actor2	--> print
 			//												actor3  --> print
 
-			//val router = system.actorOf(Props(createDiagnostic).withRouter(
-			//	BroadcastRouter(routees = routees2)), "router-to-transformers")
-			// send some text, starting the chain of events
+			val router = system.actorOf(Props(createDiagnostic).withRouter(
+				BroadcastRouter(nrOfInstances = 3)), "router")
+
+			val transformerWithRouter = system.actorOf(Props(new Transformer(router) with TimingDiagnostics), "transformer-with-router")
+			val transformer = system.actorOf(Props(new Transformer(transformerWithRouter) with TimingDiagnostics), "first-transformer")
+
+			// test printer actor verifying that the TestKit actor is being reached.
 			printer ! SomeMessage(1, "test printer")
 			expectMsg(SomeMessage(1, "test printer"))
 
-			router ! SomeMessage(1, "some message")
-			//transformer ! SomeMessage(1, "some text to play with")
-			//expectMsg(SomeMessage(1, "some text to play with"))
+			// send message to first transformer which forwards to above topology
+			transformer ! (SomeMessage(1, "some text to play with"))
+			expectMsg(SomeMessage(1, "some text to play with"))
 
 			// create promise/future pair to be used below
-			val p = Promise[Seq[DiagnosticsData[(Long, Long)]]]																				//$ These can be created and used in pairs. This is a hook into the actor eventually finishing.  This way we can hang on to results. //$
+			val p = Promise[Seq[Any]]																				//$ These can be created and used in pairs. This is a hook into the actor eventually finishing.  This way we can hang on to results. //$
 			val future = p.future
 
 			// create a return address that the spiders will send diagnostic data to. The case class here is a workaround due to erasure
 			// happening during the match in "receive"
-			case class LongLongHolder(a: DiagnosticsData[(Long, Long)])
+			//case class LongLongHolder(a: DiagnosticsData[(Long, Long)])
 			val returnAddress = system.actorOf(Props(new Actor {
 			 	var results = List[DiagnosticsData[(Long, Long)]]()
 			 	def receive = {
-			 		case m: LongLongHolder =>
-			 			results = results :+ m.asInstanceOf[DiagnosticsData[(Long, Long)]]
+			 		case m: DiagnosticsData[(Long, Long)] =>
+			 			println("got a result")
+			 			results = results :+ m
 			 			if (results.size == 6) p.success(results)								//$ Note that this is a cheat and that "in a real system you would work with what you have at a certain moment in time" //$
 			 	}
 			}))
 
 			// this is the request for diagnostics data.  It could have been sent to any actor in the web that extends the diagnostics trait.
-			printer ! (TimeDataRequest(1), Spider(returnAddress))*/
+			printer ! (TimeDataRequest(1), Spider(returnAddress))
 
 
-			//val timingData = Await.result(future, 5 seconds)
+			val timingData = Await.result(future, 5 seconds)
+			val cl = timingData(0).getClass
+			println("type")
+			println(cl)
+			//timingData.foreach {
+
+				//}
 			//timingData.map(_.data._1 must be (1))
-			//println(timingData.mkString("\n"))
+			println(timingData.mkString("\n"))
 
 
 		}
